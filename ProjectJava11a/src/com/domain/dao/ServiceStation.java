0c827fcb3e9b4efebc14c53a70da33dabc001ea2
shenderov <shenderov.k@gmail.com>
2016-04-06 21:33:18 +0300

    dao pakage added

---
+++ ProjectJava11a/src/com/domain/dao/ServiceStation.java
@@ -0,0 +1,15 @@
+package com.domain.dao;
+
+public class ServiceStation {
+String name;
+String serviceType;
+String email;
+String phone;
+String contactPerson;
+String vehicleType;
+String url;
+boolean isInsuranceCompaniesAccepted;
+String license;
+String photo;
+String password;
+}
