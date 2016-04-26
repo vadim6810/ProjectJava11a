459fbbcf10104788ef5bddfcda8e5998a4adbdf2
edward <edward@EdwardLFish>
2016-04-27 01:29:52 +0300

    Added Class Vehicle

--- ProjectJava11a/src/com/domain/dao/Vehicle.java
+++ ProjectJava11a/src/com/domain/dao/Vehicle.java
@@ -1,5 +1,55 @@
 package com.domain.dao;
 
 public class Vehicle {
+	String vendor;
+	String model;
+	String transmissionType;
+	String carBodyType;
+	int year;
+	int engineCapacity;
+
+	public Vehicle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vehicle(String vendor, String model, String transmissionType, String carBodyType, int year,
+			int engineCapacity) {
+		this.vendor = vendor;
+		this.model = model;
+		this.transmissionType = transmissionType;
+		this.carBodyType = carBodyType;
+		this.year = year;
+		this.engineCapacity = engineCapacity;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getTransmissionType() {
+		return transmissionType;
+	}
+
+	public String getCarBodyType() {
+		return carBodyType;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public int getEngineCapacity() {
+		return engineCapacity;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [vendor=" + vendor + ", model=" + model + ", transmissionType=" + transmissionType
+				+ ", carBodyType=" + carBodyType + ", year=" + year + ", engineCapacity=" + engineCapacity + "]";
+	}
 
 }
