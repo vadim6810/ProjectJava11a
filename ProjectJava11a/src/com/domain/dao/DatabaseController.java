9cb32d7ee5377fab387062ab68b1dc3dbe8030b8
edward <edward@EdwardLFish>
2016-04-29 17:02:06 +0300

    Added methods to interface IPersistenceController

--- ProjectJava11a/src/com/domain/dao/DatabaseController.java
+++ ProjectJava11a/src/com/domain/dao/DatabaseController.java
@@ -1,7 +1,83 @@
 package com.domain.dao;
 
+import java.util.Set;
+
 import com.domain.interfaces.IPersistenceController;
+import com.domain.model.Client;
+import com.domain.model.TenderRequest;
 
 public class DatabaseController implements IPersistenceController {
+
+	@Override
+	public boolean addClient(Client client) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeClient(String clientEmail) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Client getClientByEmail(String clientEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client editClient(Client client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTender(String clientEmail, TenderRequest tender) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TenderRequest getTenderById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<TenderRequest> getTendersListByClient(String clientEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TenderRequest removeTenderById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TenderRequest editTender(String clientEmail, TenderRequest tender) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<TenderRequest> getTendersListForService(String serviceEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addOfferToTender(int tenderId, String serviceEmail, float bid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeOfferFromTender(int tenderId, String serviceEmail) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- ProjectJava11a/src/com/domain/interfaces/IPersistenceController.java
+++ ProjectJava11a/src/com/domain/interfaces/IPersistenceController.java
@@ -1,5 +1,34 @@
 package com.domain.interfaces;
 
+import java.util.Set;
+
+import com.domain.model.Client;
+import com.domain.model.TenderRequest;
+
 public interface IPersistenceController {
+
+	boolean addClient(Client client);
+
+	boolean removeClient(String clientEmail);
+
+	Client getClientByEmail(String clientEmail);
+
+	Client editClient(Client client);
+
+	void addTender(String clientEmail, TenderRequest tender);
+
+	TenderRequest getTenderById(int id);
+
+	Set<TenderRequest> getTendersListByClient(String clientEmail);
+
+	TenderRequest removeTenderById(int id);
+
+	TenderRequest editTender(String clientEmail, TenderRequest tender);
+
+	Set<TenderRequest> getTendersListForService(String serviceEmail);
+
+	boolean addOfferToTender(int tenderId, String serviceEmail, float bid);
+
+	boolean removeOfferFromTender(int tenderId, String serviceEmail);
 
 }
