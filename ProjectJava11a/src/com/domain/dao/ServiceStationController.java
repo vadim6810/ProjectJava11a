2243802ed39f85ab38b8f41b80dabbd51c147439
reinsilver7777 <Bbbb@home>
2016-05-03 04:47:04 +0300

    adding 4 implement metods to ServerStation controller

--- ProjectJava11a/src/com/domain/dao/ServiceStationController.java
+++ ProjectJava11a/src/com/domain/dao/ServiceStationController.java
@@ -1,7 +1,10 @@
 package com.domain.dao;
 
-import javax.persistence.EntityManager;
+import javax.persistence.*;
 import javax.persistence.PersistenceContext;
+
+import org.hibernate.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.domain.interfaces.IServiceStation;
 import com.domain.model.ServiceStation;
@@ -12,27 +15,39 @@ public class ServiceStationController implements IServiceStation {
 	EntityManager em;
 
 	@Override
+	@Transactional
 	public boolean addServiceStation(ServiceStation servStat) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		if (em.find(ServiceStation.class, servStat.getEmail())==null){
+			em.persist(servStat);
+			res = true;
+		}
+		return res;
 	}
 
 	@Override
 	public boolean removeServiceStation(String serviceEmail) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		ServiceStation servStat = em.find(ServiceStation.class, serviceEmail);
+			if (serviceEmail !=null){
+				em.remove(servStat);
+				res = true;
+		}
+		return res;
 	}
 
 	@Override
 	public Iterable<ServiceStation> getServiceStationByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = (Query) em.createQuery("select s from ServiceStation s where s.name=?1");
+		query.setParameter(1, name);
+		return ((javax.persistence.Query) query).getResultList();
 	}
 
 	@Override
 	public Iterable<ServiceStation> getServiceStationsByRequest(String... requests) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = (Query) em.createQuery("select s from ServiceStation s where s.requests=?1");
+		query.setParameter(1, requests);
+		return ((javax.persistence.Query) query).getResultList();
 	}
 
 }
